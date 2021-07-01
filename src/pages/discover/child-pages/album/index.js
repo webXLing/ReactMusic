911a9da0b411a5d6cc136a0d03cdf4b51a6bd132
web_XL <597146220@qq.com>
2021-07-01 14:38:31 +0800

    修改打包配置

--- src/pages/discover/child-pages/album/index.js
+++ src/pages/discover/child-pages/album/index.js
@@ -2,7 +2,7 @@
  * @Author: web_XL
  * @Date: 2021-06-10 21:05:01
  * @LastEditors: web_XL
- * @LastEditTime: 2021-06-10 21:06:06
+ * @LastEditTime: 2021-07-01 14:37:51
  * @Description: 
  */
 import React, { memo } from 'react'
@@ -10,7 +10,7 @@ import React, { memo } from 'react'
 export default memo(function XLAlbum () {
   return (
     <div>
-      XLAlbum
+      XLAlbum  修改打包配置
     </div>
   )
 })
